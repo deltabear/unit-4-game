dd82e6381ae661a4bddf0f4bc5191e47107ec827
max delzer <emma.delzer@gmail.com>
2019-05-15 21:20:04 -0700

    Began pseudocoding

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Game will start when page loads
+
+//Need variables for wins and losses, and array for numbers
+
+
+//Number randomizer we can use for selecting random number on page, values for Gem Buttons
+
+//Page should display player's wins and losses next to Random NUmber
+
+//Need to set up a function with an if/else to reset the player score and game values WITHOUT refreshing
+    //Game must restart while still keeping track of current losses and wins since page opened
+
+alert("Test!")
