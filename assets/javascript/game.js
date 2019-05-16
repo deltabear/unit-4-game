81c3c398c3e38e3ff59dc8524cdc4ef6bd587c96
max delzer <emma.delzer@gmail.com>
2019-05-15 22:59:24 -0700

    Started function to call back later, game counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,15 @@
 // Game will start when page loads
 
+// startGame()
+// Its how we we will start and restart the game.
+// (Note: It's not being run here. It's just being made for future use.)
+function startGame() {
 //Need variables for wins and losses, and array for numbers
 
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var totalScore = 0;
 
 //Number randomizer we can use for selecting random number on page, values for Gem Buttons
 
@@ -10,4 +18,4 @@
 //Need to set up a function with an if/else to reset the player score and game values WITHOUT refreshing
     //Game must restart while still keeping track of current losses and wins since page opened
 
-console.log("Test!");
+console.log("Test!")
