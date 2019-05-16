f87800cfcd356c2aa43f95b2bf3485a6f10350e3
max delzer <emma.delzer@gmail.com>
2019-05-15 22:35:08 -0700

    Changed test alert to console.log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,4 +10,4 @@
 //Need to set up a function with an if/else to reset the player score and game values WITHOUT refreshing
     //Game must restart while still keeping track of current losses and wins since page opened
 
-alert("Test!")
+console.log("Test!");
