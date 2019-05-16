bf04b2793f25f70669f8f9ee69323f1ca992ce57
max delzer <emma.delzer@gmail.com>
2019-05-16 16:56:36 -0700

    Created variables for buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
 // Game will start when page loads
+var purpJewelValue = [];
+var redJewelValue = [];
+var greenJewelValue = [];
+var bluJewelValue = [];
+
+var randomNum = [];
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var totalScore = 0;
 
 // startGame()
 // Its how we we will start and restart the game.
@@ -6,17 +16,20 @@
 function startGame() {
 //Need variables for wins and losses, and array for numbers
 
-// Game counters
-var winCounter = 0;
-var lossCounter = 0;
-var totalScore = 0;
+
 
 //Number randomizer we can use for selecting random number on page, values for Gem Buttons
+  function getRandomNum() {
+    return randomNum= Math.floor(Math.random() * 100) + 1;
+  }
 
+  
 //Page should display player's wins and losses next to Random NUmber
+//When player reaches condition value equal to Random Number - victory!
+//Player score is greater then Random Number? BUST. Less then random number means game is still at hand.
 
 //Need to set up a function with an if/else to reset the player score and game values WITHOUT refreshing
     //Game must restart while still keeping track of current losses and wins since page opened
-
-console.log("Test!");
 }
+
+console.log("Hello world!");
