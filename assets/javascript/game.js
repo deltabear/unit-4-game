d2ba1f19926581026d0465ebe0ae943a97143390
max delzer <emma.delzer@gmail.com>
2019-05-16 18:58:42 -0700

    Working on math.random function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,17 +11,20 @@ var winCounter = 0;
 var lossCounter = 0;
 var totalScore = 0;
 
+let jewelNumArray = [5, 15, 25, 50, 80];
+let randomGoalArray = [160, 180, 200, 245, 300];
+
 // startGame()
 // Its how we we will start and restart the game.
 // (Note: It's not being run here. It's just being made for future use.)
 function startGame() {
 
 //Number randomizer we can use for selecting random number on page, values for Gem Buttons
-  function getRandomNum() {
-    return randomNum= Math.floor(Math.random() * 100) + 1;
-  }
+function getRandomGoal () {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-  
+
 //Page should display player's wins and losses next to Random NUmber
 //When player reaches condition value equal to Random Number - victory!
 //Player score is greater then Random Number? BUST. Less then random number means game is still at hand.
